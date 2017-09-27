c54109806e00f08ad4e1e8241306cdab202beda1
tjones61 <tjones61@10.18.12.31>
2017-09-27 14:50:42 -0500

    L3 start

---
+++ src/dip/lab3/Overlord.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab3;
+
+/**
+ *
+ * @author L115student
+ */
+public interface Overlord {
+    
+}
